b2c1c04976dc977a63fe62871029aecfa1ec1f4f
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-03-18 14:09:24 -0300

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameManager gameManager;
+    public GameObject player;
+    public GameObject mainCamera;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+    public void LoadThisScene(string sceneName)
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        if(sceneName.StartsWith("Gameplay"))
+        {
+            gameManager.gameState = GameManager.GameState.Gameplay;
+        }
+        if(sceneName == "MainMenu")
+        {
+            gameManager.gameState = GameManager.GameState.MainMenu;
+        }
+        if(sceneName.StartsWith("GameOver"))
+        {
+            gameManager.gameState = GameManager.GameState.GameOver;
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        mainCamera.GetComponent<CameraFollow>().target = player.transform;
+        player.transform.position = GameObject.FindWithTag("Spawn").transform.position;
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
