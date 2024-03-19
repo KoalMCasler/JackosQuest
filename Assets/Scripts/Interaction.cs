5d42420c326ce85f6bc2a7b3cdb3499c666f8777
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-03-19 09:28:02 -0300

    Major update

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,31 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Interaction : MonoBehaviour
 {
-   public GameObject currentInterObj = null;
+    public GameObject currentInterObj = null;
     public InteractableObject currentInterObjScript = null;
+    public TextMeshProUGUI CoinCounter;
+    public int coinCount;
+    public GameObject indicator;
 
+    void Start()
+    {
+        indicator.SetActive(false);
+        coinCount = 0;
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && currentInterObj == true)
         {
-            if (currentInterObjScript.itemType == InteractableObject.ItemType.Info)
-            {
-                currentInterObjScript.Info();
-            }
-            if (currentInterObjScript.itemType == InteractableObject.ItemType.Pickup)
-            {
-                currentInterObjScript.Pickup();
-            }
+            Interact();
         }
+        CoinCounter.text = string.Format("You have {0} coins", coinCount);
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Interactable") == true)
         {
+            indicator.SetActive(true);
             currentInterObj = other.gameObject;
             currentInterObjScript = currentInterObj.GetComponent<InteractableObject>();
         }
@@ -36,6 +40,22 @@ public class Interaction : MonoBehaviour
         if (other.CompareTag("Interactable") == true)  
         {
             currentInterObj = null;
+            indicator.SetActive(false);
         }
+    }
+    void Interact()
+    {
+        if (currentInterObjScript.itemType == InteractableObject.ItemType.Info)
+            {
+                currentInterObjScript.Info();
+            }
+            if (currentInterObjScript.itemType == InteractableObject.ItemType.Pickup)
+            {
+                if(currentInterObj.name == "Coin")
+                {
+                    coinCount += 1;
+                }
+                currentInterObjScript.Pickup();
+            }
     }
 }
